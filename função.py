c001db2e08187b7df21edd1dc1396ec470f39c65
ogygu <gustavobonjardim06@gmail.com>
2024-11-11 13:54:26 -0300

    changes

---
+++ função.py
@@ -0,0 +1,77 @@
+"""
+#1
+def verificação():
+    x = int(input('digite um numero :')) 
+    if x % 2 == 0 :
+        print(f"{x} é um numero par ")
+        return True
+    else :
+        print(f"{x} é impar")
+        return False
+verificação()"""
+"""
+#2
+def tamanho(x,y):
+    if x > y :
+        print(f"{x} é maior que {y}")
+    elif x == y :
+        print(f"{x} e {y} são iguais ")
+    else:
+        print(f"{y} é maior que {x}")
+
+num1 = int(input("digite um numero :"))
+num2 = int(input("digite um numero :"))
+
+tamanho(num1,num2)"""
+
+"""
+#3
+def soma(a , b):
+
+    soma1 = sum(range(a+1, b))
+    return soma1
+      
+       
+
+num1 = int(input("digite um numero :"))
+num2 = int(input("digite um numero :"))   
+
+resultado = soma(num1,num2)
+print(resultado)"""
+
+"""
+#4
+def multiplicação(x,y):
+    mult = x * y
+    return mult
+   
+   
+
+num1 = int(input("digite um numero :"))
+num2 = int(input("digite um numero :"))  
+
+resultado = multiplicação(num1,num2)
+print(resultado)"""
+
+#5
+"""
+#6
+def potencia(x,y):
+    Potencia = x**y
+    return Potencia
+
+num1 = int(input("digite um numero :"))
+num2 = int(input("digite um numero :"))  
+
+resultado = potencia(num1,num2)
+print(resultado)"""
+
+def verificar_numeroPrimo(x,y):
+    
+
+
+
+    
+
+
+
